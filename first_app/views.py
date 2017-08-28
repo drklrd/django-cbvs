8e5c3568f1b45978611c40f3934f4caf929d3a1b
drklrd <srvbhattarai@gmail.com>
2017-08-28 21:24:45 +0545

    move templates to individual app folder

--- first_app/views.py
+++ first_app/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 def index(request):
     my_dict = {'insert_me': 'Hello from views.py!'}
-    return render(request,'index.html',context=my_dict)
+    return render(request,'first_app/index.html',context=my_dict)
