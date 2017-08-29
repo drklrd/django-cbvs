0d868fa2458b4ed7e9c0d4f99c177f175df3c21b
drklrd <srvbhattarai@gmail.com>
2017-08-29 21:13:09 +0545

    form validation

--- first_app/forms.py
+++ first_app/forms.py
@@ -1,15 +1,28 @@
 from django import forms
 from django.core import validators
 
+
 class FormName(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
+    verify_email = forms.EmailField(label='Enter email again')
     text = forms.CharField(widget=forms.Textarea)
-    botcatcher = forms.CharField(
-        required=False,
-        widget=forms.HiddenInput,
-        validators=[validators.MaxLengthValidator(0)]
-    )
+
+    def clean(self):
+        # return all clean data from the Form
+        print('*******************')
+        all_clean_data = super().clean()
+        email = all_clean_data['email']
+        vmail = all_clean_data['verify_email']
+
+        if email != vmail:
+            raise forms.ValidationError("Make sure emails match !")
+
+    # botcatcher = forms.CharField(
+    #     required=False,
+    #     widget=forms.HiddenInput,
+    #     validators=[validators.MaxLengthValidator(0)]
+    # )
 
 
     # def clean_botcatcher(self):
