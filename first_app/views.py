0638318a256d8e4a850b5ed13d5e610fa24330d6
drklrd <srvbhattarai@gmail.com>
2017-08-29 20:54:10 +0545

    add basic form

--- first_app/views.py
+++ first_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from first_app.models import Topic,Webpage,AccessRecord
-
+from . import forms
 # Create your views here.
 
 
@@ -10,3 +10,15 @@ def index(request):
     date_dict = {'access_records' : webpages_list }
     # my_dict = {'insert_me': 'Hello from views.py!'}
     return render(request,'first_app/index.html',context=date_dict)
+
+def form_name_view(request):
+    form = forms.FormName()
+
+    if request.method == 'POST':
+        form = forms.FormName(request.POST)
+
+        if form.is_valid():
+            print("Validaion")
+            print("NAME " + form.cleaned_data['name'])
+
+    return render(request,'first_app/form_page.html',{'form':form})
