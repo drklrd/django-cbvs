8aba702d1211e3f0d3aa1ad87fc52b8bcdd21c56
drklrd <srvbhattarai@gmail.com>
2017-08-29 21:36:35 +0545

    add model form

--- first_app/forms.py
+++ first_app/forms.py
@@ -1,32 +1,42 @@
 from django import forms
-from django.core import validators
+from first_app.models import Topic
+
+class NewTopicForm(forms.ModelForm):
+
+    class Meta():
+        model = Topic
+        fields = '__all__'
 
 
-class FormName(forms.Form):
-    name = forms.CharField()
-    email = forms.EmailField()
-    verify_email = forms.EmailField(label='Enter email again')
-    text = forms.CharField(widget=forms.Textarea)
 
-    def clean(self):
-        # return all clean data from the Form
-        print('*******************')
-        all_clean_data = super().clean()
-        email = all_clean_data['email']
-        vmail = all_clean_data['verify_email']
-
-        if email != vmail:
-            raise forms.ValidationError("Make sure emails match !")
-
-    # botcatcher = forms.CharField(
-    #     required=False,
-    #     widget=forms.HiddenInput,
-    #     validators=[validators.MaxLengthValidator(0)]
-    # )
-
-
-    # def clean_botcatcher(self):
-    #     botcatcher = self.cleaned_data['botcatcher']
-    #     if len(botcatcher) > 0 :
-    #         raise forms.ValidationError("Gotcha Bot !")
-    #     return botcatcher
+# from django import forms
+# from django.core import validators
+#
+#
+# class FormName(forms.Form):
+#     name = forms.CharField()
+#     email = forms.EmailField()
+#     verify_email = forms.EmailField(label='Enter email again')
+#     text = forms.CharField(widget=forms.Textarea)
+#
+#     def clean(self):
+#         # return all clean data from the Form
+#         all_clean_data = super().clean()
+#         email = all_clean_data['email']
+#         vmail = all_clean_data['verify_email']
+#
+#         if email != vmail:
+#             raise forms.ValidationError("Make sure emails match !")
+#
+#     # botcatcher = forms.CharField(
+#     #     required=False,
+#     #     widget=forms.HiddenInput,
+#     #     validators=[validators.MaxLengthValidator(0)]
+#     # )
+#
+#
+#     # def clean_botcatcher(self):
+#     #     botcatcher = self.cleaned_data['botcatcher']
+#     #     if len(botcatcher) > 0 :
+#     #         raise forms.ValidationError("Gotcha Bot !")
+#     #     return botcatcher
--- first_app/views.py
+++ first_app/views.py
@@ -2,23 +2,38 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from first_app.models import Topic,Webpage,AccessRecord
 from . import forms
+from first_app.forms import NewTopicForm
 # Create your views here.
 
 
 def index(request):
-    webpages_list = AccessRecord.objects.order_by('date')
-    date_dict = {'access_records' : webpages_list }
+    webpages_list = Topic.objects.all()
+    date_dict = {'topics' : webpages_list }
     # my_dict = {'insert_me': 'Hello from views.py!'}
     return render(request,'first_app/index.html',context=date_dict)
 
 def form_name_view(request):
-    form = forms.FormName()
+
+    form = NewTopicForm()
 
     if request.method == 'POST':
-        form = forms.FormName(request.POST)
+        form = NewTopicForm(request.POST)
 
         if form.is_valid():
-            print("Validaion")
-            print("NAME " + form.cleaned_data['name'])
+            form.save(commit=True)
+            return index(request)
+        else:
+            print('Error form invalid')
 
     return render(request,'first_app/form_page.html',{'form':form})
+
+    # form = forms.FormName()
+    #
+    # if request.method == 'POST':
+    #     form = forms.FormName(request.POST)
+    #
+    #     if form.is_valid():
+    #         print("Validaion")
+    #         print("NAME " + form.cleaned_data['name'])
+    #
+    # return render(request,'first_app/form_page.html',{'form':form})
