c8c5ecbe681cf9bb47581532ec4f3787c4f29825
drklrd <srvbhattarai@gmail.com>
2017-09-04 21:37:40 +0545

    add few templates and some necessary functions

--- blog-project/mysite/blog/views.py
+++ blog-project/mysite/blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_list_or_404,redirect
+from django.utils import timezone
 from django.views.generic import (TemplateView,ListView,DetalView,CreateView,UpdateView,DeleteView)
 from blog.models import Post,Comment
 from blog.forms import PostForm,CommentForm
@@ -43,3 +44,37 @@ class DraftListView(LoginRequiredMixin,ListView):
 
     def get_queryset(self): # allows to use django ORM when dealing with generic views. Refer django  documentation for more information
         return Post.objects.filter(published_date__isnull=True).order_by('created_date')
+
+
+@login_required
+def post_publish(request,pk):
+    post = get_list_or_404(Post,pk=pk)
+    post.publish()
+    return redirect('post_detail',pk=pk)
+
+@login_required
+def add_comment_to_post(request,pk):
+    post = get_list_or_404(Post,pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail',pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request,'blog/comment_form.html',{'form':form})
+
+@login_required
+def comment_approve(request,pk):
+    comment = get_list_or_404(Comment,pk=pk)
+    comment.approve()
+    return redirect('post_detail',pk=comment.post.pk)
+
+@login_required
+def comment_remove(request,pk):
+    comment = get_list_or_404(Comment,pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail',pk=comment.post.pk)
