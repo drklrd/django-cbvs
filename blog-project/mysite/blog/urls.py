191d8654f35bccecfba44cddb977c5207cea89cb
drklrd <srvbhattarai@gmail.com>
2017-09-06 06:26:23 +0545

    complete blog project

--- blog-project/mysite/blog/urls.py
+++ blog-project/mysite/blog/urls.py
@@ -4,7 +4,7 @@ from blog import views
 urlpatterns = [
     url(r'^$',views.PostListView.as_view(),name='post_list'),
     url(r'^about/$',views.AboutView.as_view(),name='about'),
-    url(r'^post/(?P<pk>\d+)$',views.DetalView.as_view(),name='post_detail'),
+    url(r'^post/(?P<pk>\d+)$',views.PostDetailView.as_view(),name='post_detail'),
     url(r'^post/new/$',views.CreatePostView.as_view(),name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$',views.PostUpdateView.as_view(),name='post_edit'),
     url(r'^post/(?P<pk>\d+)/remove/$',views.PostDeleteView.as_view(),name='post_remove'),
@@ -13,5 +13,5 @@ urlpatterns = [
     url(r'^comment/(?P<pk>\d+)/approve/$',views.comment_approve,name='comment_approve'),
     url(r'^comment/(?P<pk>\d+)/remove/$',views.comment_remove,name='comment_remove'),
     url(r'^post/(?P<pk>\d+)/publish/$',views.post_publish,name='post_publish'),
-    
+
 ]
--- blog-project/mysite/blog/views.py
+++ blog-project/mysite/blog/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render,get_list_or_404,redirect
+from django.shortcuts import render,get_object_or_404,redirect
 from django.utils import timezone
-from django.views.generic import (TemplateView,ListView,DetalView,CreateView,UpdateView,DeleteView)
+from django.views.generic import (TemplateView,ListView,DetailView,CreateView,UpdateView,DeleteView)
 from blog.models import Post,Comment
 from blog.forms import PostForm,CommentForm
 from django.urls import reverse_lazy
@@ -17,8 +17,9 @@ class PostListView(ListView):
     def get_queryset(self): # allows to use django ORM when dealing with generic views. Refer django  documentation for more information
         return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 
-class PostDetailView(DetalView):
+class PostDetailView(DetailView):
     model = Post
+
 
 class CreatePostView(LoginRequiredMixin, CreateView): # login_required decorator work for function based views only. so to use it for CBV we use mixins
     login_url = '/login/'
@@ -48,13 +49,13 @@ class DraftListView(LoginRequiredMixin,ListView):
 
 @login_required
 def post_publish(request,pk):
-    post = get_list_or_404(Post,pk=pk)
+    post = get_object_or_404(Post,pk=pk)
     post.publish()
     return redirect('post_detail',pk=pk)
 
 @login_required
 def add_comment_to_post(request,pk):
-    post = get_list_or_404(Post,pk=pk)
+    post = get_object_or_404(Post,pk=pk)
     if request.method == 'POST':
         form = CommentForm(request.POST)
         if form.is_valid():
@@ -68,13 +69,13 @@ def add_comment_to_post(request,pk):
 
 @login_required
 def comment_approve(request,pk):
-    comment = get_list_or_404(Comment,pk=pk)
+    comment = get_object_or_404(Comment,pk=pk)
     comment.approve()
     return redirect('post_detail',pk=comment.post.pk)
 
 @login_required
 def comment_remove(request,pk):
-    comment = get_list_or_404(Comment,pk=pk)
+    comment = get_object_or_404(Comment,pk=pk)
     post_pk = comment.post.pk
     comment.delete()
     return redirect('post_detail',pk=comment.post.pk)
